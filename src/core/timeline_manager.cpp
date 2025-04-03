093d67eda4beb035b806065483445ec48ac50029
ちぇけガール <131373665+chekegirl@users.noreply.github.com>
2025-04-03 23:20:40 +0900

    Add files via upload

--- src/core/timeline_manager.cpp
+++ src/core/timeline_manager.cpp
@@ -14,20 +14,42 @@
 #include <core/project_data.hpp>
 #include <core/timeline_manager.hpp>
 
-std::vector<std::vector<Clip_t>> layer_effects;
+// 全レイヤ、全クリップのリスト
+std::vector<std::vector<Clip_t>> layers;
 int recent_frame;
 
 // これを読んだら、そのフレームでどのエフェクトを処理すればいいか分かる。
 std::vector<Clip_t> get_frame_effects(){
 	std::vector<Clip_t> layer_frame_effects = {};
-	for (int i=0; i<layer_effects.size(); i++){
-		for (int j=0; j<layer_effects[i].size(); j++){
-			if (layer_effects[i][j].frame_start <= recent_frame && recent_frame < layer_effects[i][j].frame_end){
-				if (layer_effects[i][j].clip_type == ClipType_t::Effect){
-					layer_frame_effects.push_back(layer_effects[i][j]);
+	for (int i=0; i<layers.size(); i++){
+		for (int j=0; j<layers[i].size(); j++){
+			if (layers[i][j].frame_start <= recent_frame && recent_frame <= layers[i][j].frame_end){
+				if (layers[i][j].clip_type == ClipType_t::Effect){
+					layer_frame_effects.push_back(layers[i][j]);
 				}
 			}
 		}
 	}
 	return (layer_frame_effects);
+}
+
+// clipを何処かのレイヤに追加する関数
+void add_clip(Clip_t new_clip){
+	if (layers.size() < clip.layer){
+		while(layers.size() < clip.layer){
+			std::vector<Clip_t> new_layer = {};
+			new_layer.push_back(clip);
+			layers.append(new_layer);
+		}
+	}else{
+		bool flg01 = false;
+		for(int i=0; i<layers[new_clip.layer].size(); i++){
+			if ((layers[i].frame_start <= new_clip.frame_start && new_clip.frame_start <= layers[i].frame_end) || (layers[i].frame_start <= new_clip.frame_end && new_clip.frame_end <= layers[i].frame_end)){
+				flg01 = true;
+			}
+		}
+		if (flg01 == false){
+			layers[new_clip.layer].push_back(new_clip);
+		}
+	}
 }
