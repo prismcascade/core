aa57fec2df63a6d5e985de736c3ad4827870a66e
yukatayu <yukatayu.dev@gmail.com>
2025-04-04 01:33:01 +0900

    add: build scripts: sum plugin

--- scripts_dev/linux_build_dll.bash
+++ scripts_dev/linux_build_dll.bash
@@ -5,6 +5,7 @@ cd ..
 mkdir -p dll_build
 
 cp sample_plugins/twice_plugin.cpp dll_build/
+cp sample_plugins/sum_plugin.cpp dll_build/
 cp src/core/project_data.hpp dll_build/
 
 pushd dll_build
@@ -12,5 +13,9 @@ pushd dll_build
 clang++ -std=c++20 -O2 -g -shared twice_plugin.cpp -o debug_twice_plugin.so && \
 mkdir -p ../build/tests/plugins && \
 cp debug_twice_plugin.so ../build/tests/plugins/
+
+clang++ -std=c++20 -O2 -g -shared sum_plugin.cpp -o debug_sum_plugin.so && \
+mkdir -p ../build/tests/plugins && \
+cp debug_sum_plugin.so ../build/tests/plugins/
 
 popd
