8602aa6bcb2546eea096f9cfb8d8299492aca59b
yukatayu <yukatayu.dev@gmail.com>
2025-04-21 16:51:32 +0900

    add: placeholder

--- scripts_dev/win_run.bat
+++ scripts_dev/win_run.bat
@@ -7,5 +7,12 @@ chcp 65001
 cmake ..
 cmake --build . --parallel --config RelWithDebInfo
 
-.\cli\RelWithDebInfo\prismcascade_cli.exe
 
+rem ===== run all test executables =====
+for %%f in (tests\RelWithDebInfo\*.exe) do (
+    echo Running %%f
+    call "%%f"
+)
+
+rem ===== run CLI =====
+call cli\RelWithDebInfo\prismcascade_cli.exe
