2d0b77142848262be8a0172e064f325dbb50f8ad
yukatayu <yukatayu.dev@gmail.com>
2025-05-18 14:29:42 +0900

    add: node

---
+++ include/prismcascade/ast/node.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <memory>
+#include <optional>
+#include <prismcascade/common/types.hpp>
+#include <prismcascade/memory/time.hpp>
+#include <prismcascade/memory/types_internal.hpp>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace prismcascade::ast {
+
+struct AstNode;
+struct SubEdge {
+    std::weak_ptr<AstNode> source;
+    std::uint64_t          output_index{};
+};
+
+struct AstNode {
+    struct SubOutputRef {
+        std::uint64_t          src_index;  // this‐node の副出力 index
+        std::weak_ptr<AstNode> dst_node;   // 参照しているノード
+        std::uint64_t          dst_slot;   // dst_node の入力スロット
+    };
+
+    struct InputWindow {
+        std::int64_t           look_behind = 0;
+        std::int64_t           look_ahead  = 0;
+        memory::rational_fps_t fps{};
+    };
+
+    // 入力
+    // TODO: 将来的にはVectorParam に即値を入れられるようにする？
+    using input_t = std::variant<std::monostate,
+                                 std::shared_ptr<AstNode>,  // メイン出力を通じた AST 接続
+                                 SubEdge,                   // サブ出力参照
+                                 std::int64_t, bool, double, std::string>;
+
+    // handler は実行ごとに変化する，勝手につけた通し番号
+    std::uint64_t plugin_handler{};
+    std::uint64_t plugin_instance_handler{};
+    // uuidは，プラグイン側で決められる識別ID
+    std::string plugin_uuid{};
+
+    // メタデータからも辿れるが，処理の途中で途中で変化しない値なので，扱いやすさのためにもコピーをここに置く
+    std::int32_t protocol_version{};
+    PluginType   plugin_type{};
+    std::string  plugin_name{};
+
+    // 主出力先
+    std::weak_ptr<AstNode> parent;
+    // 副出力先 (src_index, dst_node, dst_index)
+    std::vector<SubOutputRef> sub_output_references;
+
+    // 入力
+    std::vector<input_t> inputs;
+    // lookBehind, lookAhead
+    std::vector<std::optional<InputWindow>> input_window;
+
+    // DLL側のメモリ
+    std::shared_ptr<memory::ParameterPackMemory> input_parameters;
+    std::shared_ptr<memory::ParameterPackMemory> output_parameters;
+
+    // Clip として振る舞う場合
+    std::optional<VideoMetaData> video_meta;
+    std::optional<AudioMetaData> audio_meta;
+
+    // 直接操作してもよいが，便利メソッドとして定義
+    void resize_inputs(const std::vector<std::vector<VariableType>>& types);
+    void resize_outputs(const std::vector<std::vector<VariableType>>& types);
+};
+
+}
